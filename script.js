665905135cf8d0d4699267823c37bec843e64903
swcodes <87975571+sw-codes@users.noreply.github.com>
2025-03-27 14:30:18 +0000

    add book functionality implemented
    added a form for a user to add new book information to the library

--- script.js
+++ script.js
@@ -1,39 +1,75 @@
-let libraryDiv = document.querySelector(".library");
+const libraryDiv = document.querySelector(".library");
+const newBookButton = document.querySelector(".new-book-button");
+const formDiv = document.querySelector(".new-book-form");
+const form = document.querySelector("#new-book-form");
+const submitBookButton = document.querySelector(".submit-book-button");
 
 const myLibrary = [
     {
         id: crypto.randomUUID(),
         title: "boop",
-        author: "beep"
+        author: "beep",
+        numOfPages: 10
     },
     {
         id: crypto.randomUUID(),
         title: "boop",
-        author: "bop"
+        author: "bop",
+        numOfPages: 10
     },
     {
         id: crypto.randomUUID(),
         title: "bap",
-        author: "beep"
+        author: "beep",
+        numOfPages: 10
     },
 ];
 
-function Book(id, title, author) {
+function Book(id, title, author, numOfPages) {
     this.id = id,
-    this.title = title,
-    this.author = author
+        this.title = title,
+        this.author = author,
+        this.numOfPages = numOfPages
 }
 
-function addBookToLibrary(title, author) {
-    myLibrary.push(new Book(crypto.randomUUID(), title, author));
+newBookButton.addEventListener("click", () => {
+    displayForm();
+})
+
+function displayForm() {
+    formDiv.removeAttribute("hidden");
+}
+
+function addBookToLibrary(title, author, numOfPages) {
+    myLibrary.push(new Book(crypto.randomUUID(), title, author, numOfPages));
 }
 
 function displayLibrary() {
+    while (libraryDiv.lastChild) {
+        libraryDiv.removeChild(libraryDiv.lastChild);
+    }
     let libraryLength = myLibrary.length;
     for (let i = 0; i < libraryLength; i++) {
+        let newDiv = document.createElement('div');
         let newP = document.createElement('p');
-        newP.textContent = myLibrary[i].title + " by " + myLibrary[i].author + ". id: " + myLibrary[i].id;
-        libraryDiv.appendChild(newP);
+        newP.textContent = myLibrary[i].title + " by " + myLibrary[i].author + ". page count: " + myLibrary[i].numOfPages + ". id: " + myLibrary[i].id;
+        newDiv.appendChild(newP);
+        libraryDiv.appendChild(newDiv);
     }
 }
 displayLibrary();
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputBookTitle = document.querySelector("#book-title").value;
+    const inputBookAuthor = document.querySelector("#book-author").value;
+    const inputBookNumOfPages = document.querySelector("#num-of-pages").value;
+    addBookToLibrary(
+        title = inputBookTitle,
+        author = inputBookAuthor,
+        numOfPages = inputBookNumOfPages
+    )
+    console.log(myLibrary);
+    displayLibrary();
+})
